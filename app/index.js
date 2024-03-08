4c1b988300f73a93f1279c241a49c14ccf80f2bc
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2024-03-08 19:19:51 +0100

    chore: remove hardcoded port

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { v4: uuid } = require("uuid");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
